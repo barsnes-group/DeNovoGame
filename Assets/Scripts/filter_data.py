94f4236b88b9ba9a534af261618b6296dbd7797a
ingridliabakk <ier008@uib.no>
2021-09-25 15:16:42 +0200

    small changes

--- Assets/Scripts/filter_data.py
+++ Assets/Scripts/filter_data.py
@@ -1,39 +1,42 @@
 import csv
-from os import write
 import matplotlib.pyplot as plt
 
-def percentage(part : float, whole : float):
-    return 100 * part/whole
+def read_write_file(file_in, file_out, percentage):
+    mz = []
+    intensity = []
+    int_graph = []
 
-mz = []
-intensity = []
-int_graph = []
-filtered_graph = []
-
-with open('code/selected_spectra.csv', 'r') as csvfile:
-    reader = csv.reader(csvfile, delimiter=' ')
-    for row in reader:  
-        intensity.append(float(row[1]))
-        
-        max_intensity = max(intensity)
-        threshold_percentage = (float(row[1])/max_intensity*100)
-        
-        if threshold_percentage >= 5:
-            mz.append(float(row[0]))
-            int_graph.append(float(row[1]))   
-
+    with open(file_in, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ')
+        for row in reader:  
+            intensity.append(float(row[1]))
             
-plt.bar(mz, int_graph)
-plt.xlabel('m/z')
-plt.ylabel('int')
+            max_intensity = max(intensity)
+            threshold_percentage = (float(row[1])/max_intensity*100)
+            
+            if threshold_percentage >= percentage:
+                mz.append(float(row[0]))
+                int_graph.append(float(row[1]))   
 
-plt.legend()
-#plt.show()
-
-
-with open('test.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile)
+                
+    print(f"Number of peaks: {len(mz)}")
     
-    for w in range(len(mz)):
-        writer.writerow([mz[w], int_graph[w]])
-    csvfile.close()
+    #plot graph
+    plt.bar(mz, int_graph)
+    plt.xlabel('m/z')
+    plt.ylabel('int')
+    plt.legend()
+    plt.show()
+    
+    #write to file
+    '''
+    with open(filename_out, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        
+        for w in range(len(mz)):
+            writer.writerow([mz[w], int_graph[w]])
+        csvfile.close()
+    '''
+    
+    
+read_write_file('Assets/Scripts/selected_spectra.csv', ' ', 5)
